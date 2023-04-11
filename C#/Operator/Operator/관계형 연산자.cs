89c5a15c5e9f212ceb9bc10826ce8f3f699233b6
h0o0n <khnam022@naver.com>
2023-04-12 08:42:50 +0900

    C# 교과서 마스터하기 11 11. 관계형 연산자와 논리 연산자 사용하기

---
+++ C#/Operator/Operator/관계형 연산자.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator
+{
+    class 관계형_연산자
+    {
+        public void 관계형연산자()
+        {
+            //관계형(비교) 연산자:
+            //>, <, >=, <=, ==, !==
+            int firstNum = 10;
+            int secondNum = 5;
+            bool r = false;
+
+            Console.WriteLine(firstNum > secondNum); // true
+
+            r = (firstNum <= secondNum); //false
+            Console.WriteLine(r);
+
+            r = (firstNum == secondNum); // C Family : == 로 같은지 확인
+            Console.WriteLine(r);              // false
+
+            r = (firstNum != secondNum); // C Family : == 로 같은지 확인
+            Console.WriteLine(r);             // true
+        }
+    }
+}
---
+++ C#/Operator/Operator/논리연산자.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator
+{
+    class 논리연산자
+    {
+        public void 논리연산()
+        {
+            int i = 3;
+            int j = 5;
+            bool r = false;
+
+            //&& 연산자 (AND) : 둘다 참일때에만 참, 즉, 하나라도 거짓이면 거짓
+            //|| 연산자 (OR) : 하나라도 참이면 참, 즉 ,둘 다 거짓일때에만 거짓
+
+            r = (i == 3) && (j != 3);
+            Console.WriteLine(r);
+
+            r = (i == 3) || (j != 3);
+            Console.WriteLine(r);
+
+            r = (i >= 5);
+            Console.WriteLine("{0}",r);
+            Console.WriteLine("{0}", !r); // r = false 였는데 ! 연산자를 통해 true로 변환
+        }
+    }
+}
