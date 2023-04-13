1256ecc42e0e21c685f1e6937ebb3a155580ecef
h0o0n <khnam022@naver.com>
2023-04-14 08:34:22 +0900

    C# 교과서 마스터하기 31 31. 알고리즘과 절차 지향 프로그래밍

---
+++ C#/Operator/Operator/알고리즘/알고리즘.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator.알고리즘
+{
+    class 알고리즘
+    {
+        public void algo()
+        {
+            int[] data = { 3, 2, 1, 5, 4 };
+
+            data.OrderBy(d => d);
+            data.OrderByDescending(x => x);
+            
+            foreach(int d in data.OrderByDescending(x => x))
+            {
+                Console.WriteLine(d);
+            }
+
+            for(int i = 0; i <data.Length; i++)
+            {
+                for(int j=i+1; j<data.Length; j++)
+                {
+                    if(data[j] > data[i])
+                    {
+                        int temp = data[i];
+                        data[i] = data[j];
+                        data[j] = temp;
+                    }
+                }
+            }
+
+            foreach(int d in data)
+            {
+                Console.WriteLine(d);
+            }
+           
+        }
+    }
+}
