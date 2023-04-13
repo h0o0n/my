bddf7b58330fdd5d01d9b57470b03cc7622bbc1b
h0o0n <khnam022@naver.com>
2023-04-13 10:54:00 +0900

    C# 교과서 마스터하기 28 28. 제네릭 사용하기

---
+++ C#/Operator/Operator/제네릭/Dictionary.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator.제네릭
+{
+    class Dictionary
+    {
+        public void dictionary()        //키와 값을 쌍으로 관리 HashMap 과 비슷 .
+        {
+            Dictionary<int, string> todos = new Dictionary<int, string>();
+            todos.Add(1, "C#");
+            todos.Add(2, "ASP.NET");
+            todos.Add(3, "Java");
+
+            String result;
+
+            for(int i=1; i<=todos.Count; i++)
+            {
+                todos.TryGetValue(i, out result);
+                Console.WriteLine(result);
+
+            }
+        }
+    }
+}
---
+++ C#/Operator/Operator/제네릭/GenericDemo.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator.제네릭
+{
+    class GenericDemo
+    {
+        public void genericDemo()
+        {
+            Stack stack = new Stack();                  //Object 타입으로 받고 Integer로 출력   box 에 포장해서 unboxing 하는 개념
+            stack.Push(100);
+            Console.WriteLine(stack.Pop());
+
+            Stack<int> stacks = new Stack<int>(); //Integer 타입으로 받고 Integer로 출력 100이란 값을 그대로 담아서 출력 -> 속도면에서 이득
+            stacks.Push(100);
+            Console.WriteLine(stacks.Pop());
+        }
+
+        public void listDemo()
+        {
+            List<int> numbers = new List<int>();
+            numbers.Add(100);
+            numbers.Add(200);
+            numbers.Add(300);
+
+            foreach(int num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+
+        }
+
+        public void stringList()
+        {
+            List<string> color = new List<string>();
+         
+            color.Add("red");
+            color.Add("green");
+            color.Add("green");
+            color.AddRange(color);
+
+            foreach(string colors in color)
+            {
+                Console.WriteLine(colors);
+            }
+        }
+        public void Enum()
+        {
+            List<int> numbers = new List<int>();
+
+            numbers.AddRange(Enumerable.Range(1, 10)); // 1부터 10까지 저장하는 메서드
+            numbers.AddRange(Enumerable.Repeat(1, 10)); // 1을 10번 반복저장하는 메서드
+            foreach(int num in numbers)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
