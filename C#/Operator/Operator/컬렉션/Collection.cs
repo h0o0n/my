bd1cd3914bc6dcb98c0190faf34e1c8b1a56138a
h0o0n <khnam022@naver.com>
2023-04-13 09:53:15 +0900

    C# 교과서 마스터하기 27 27. 컬렉션 사용하기

---
+++ C#/Operator/Operator/컬렉션/Collection.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator.컬렉션
+{
+    class Collection
+    {
+        public void collection()
+        {
+            int number = 1_234;
+            Console.WriteLine(number);
+
+            //[2]Array
+            string[] colors = { "red", "green", "blue" };
+            Console.WriteLine(colors[0]);
+            Console.WriteLine(colors[1]);
+            Console.WriteLine(colors[2]);
+            Console.WriteLine(colors[10]); // 인덱스가 배열 범위 벗어난 에러     
+            Array.Sort(colors);
+            foreach(var color in colors){
+                Console.WriteLine(color);
+            }
+
+            Array.Reverse(colors);
+            foreach(var color in colors)
+            {
+                Console.WriteLine(color);
+            }
+
+        }
+        
+        public void collectionDemo()
+        {
+            Stack stack = new Stack();
+            stack.Push(100);
+            stack.Push(200);
+            Console.WriteLine(stack.Pop()); 
+            Console.WriteLine(stack.Pop()); 
+        }
+
+        public void queueDemo()
+        {
+            Queue queue = new Queue();
+            queue.Enqueue(100);
+            queue.Enqueue(200);
+
+            Console.WriteLine(queue.Dequeue());
+            Console.WriteLine(queue.Dequeue());
+            
+        }
+    }
+}
---
+++ C#/Operator/Operator/컬렉션/List.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Operator.컬렉션
+{
+    class List
+    {
+        public void arrayList()
+        {
+            ArrayList list = new ArrayList();
+            list.Add(100);
+            list.Add(100);
+            list.RemoveAt(1);
+            list.Add(200);
+            Console.WriteLine(list[0]);
+            Console.WriteLine(list[1]);
+            list.Insert(0, 50);
+
+            foreach(int li in list)
+            {
+                Console.WriteLine(li);
+            }
+
+        }
+
+        public void hashTable()
+        {
+            Hashtable hashtable = new Hashtable();
+            hashtable[0] = "DotNetKorea";
+            hashtable["NickName"] = "RedPlus";
+
+            Console.WriteLine(hashtable[0]);
+            Console.WriteLine(hashtable["NickName"]);
+        }
+    }
+}
