885f83706eb49e436db06e7d313ab6d7a3dc0543
h0o0n <khnam022@naver.com>
2023-04-14 09:14:49 +0900

    C# 교과서 마스터하기 32 32. 개체 만들기

---
+++ C#/Operator/Operator/객체/ObjectDemo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Class1
+{
+    public class Cars
+    { 
+        public void Go() => Console.WriteLine("차가 출발합니다.");
+        public override string ToString() => "My Car";
+
+    }
+
+    public class ObjectTest
+    {
+        Cars ca = new Cars();
+    }
+}
