9f526c15d5ed2104c0fab3762eab0eab20a20e09
h0o0n <khnam022@naver.com>
2023-04-14 10:01:46 +0900

    C# 교과서 마스터하기 34 34. 필드(Field)

---
+++ C#/Operator/Operator/필드/FieldDemo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator.필드
+{
+    public class Say
+    {
+        private string message = "반갑습니다."; // 필드
+        public void Hi()
+        {
+            Console.WriteLine(this.message);
+        }
+
+    }
+    class FieldDemo
+    {
+        private static string message = "안녕하세요";
+        public static void Hi() => Console.WriteLine(message);
+        public void main1() {
+            int number = 1_234;
+            Console.WriteLine(number);
+            Console.WriteLine(message);
+            Hi();
+            Say say = new Say();
+            say.Hi();
+        }
+        
+    }
+
+    public class Person
+    {
+        private string name = "기훈";
+        private const int m_age = 21;
+        private readonly string NickName = "DotNet";
+        private String[] websties = {"닷넷 코리아", " 비주얼 아카데미 "};
+
+        public void Show() => Console.WriteLine($"이름 : {name} \b 나이: {m_age} \b 별명: {NickName} \b 웹사이트: {string.Join(",",websties)}");
+        
+    }
+}
