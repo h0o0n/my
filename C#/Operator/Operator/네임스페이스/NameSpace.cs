bd4632ed4ab53685918f7e5a66a5b84396b94110
h0o0n <khnam022@naver.com>
2023-04-14 10:01:29 +0900

    C# 교과서 마스터하기 33 33. 네임스페이스

---
+++ C#/Operator/Operator/네임스페이스/NameSpace.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Foo;
+
+namespace Foo
+{
+    public class Car
+    {
+        public void Go() => Console.WriteLine("Foo.Car 자동차가 달립니다.");
+    }
+}
+namespace Operator.네임스페이스
+{
+    class NameSpace
+    {   
+        //클래스 이름 충돌 방지 목적
+        public void NameSpaceDemo()
+        {
+            Console.WriteLine("네임스페이스");
+            Car car = new Car();
+            car.Go();
+        }
+    }
+}
