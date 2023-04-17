81eb0b3c4d16e76ae15641684e95f07be95306ef
h0o0n <khnam022@naver.com>
2023-04-17 15:21:13 +0900

    C# 교과서 마스터하기 38  38. 속성(Property, 프로퍼티) 사용하기

---
+++ C#/Operator/Operator/속성사용/NoName.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator.속성사용
+{
+    class NoName
+    {
+        public void main()
+        {
+            var person = new { Name = "Red", Age = 21 }; //클래스를 만들지않고 익명형식
+            Console.WriteLine(person.Name);
+            Console.WriteLine(person.Age);
+        }
+        
+
+    }
+}
---
+++ C#/Operator/Operator/속성사용/Property.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator.속성사용
+{
+    class Property
+    {
+        public void main()
+        {
+            Car car = new Car();
+            car.Name = "장난감 자동차";
+            Console.WriteLine(car.Name);
+
+            Car car2 = new Car() { Name = "장난감 자동차2" }; //선언과 동시에  초기화 시켜줄 수 있다.
+            Console.WriteLine(car2.Name);
+
+            Console.WriteLine($"{nameof(car.Name)} : {car.Name}");
+            Console.WriteLine($"{nameof(car2.Name)} : {car2.Name}");
+
+        }
+    }
+
+    class Car
+    {
+ 
+        public string Name
+        {
+            get; set;
+        }
+    }
+
+
+}
