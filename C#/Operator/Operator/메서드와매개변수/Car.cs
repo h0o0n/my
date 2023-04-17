7582ffd1ccbf3ee347bcb1df8ebd86fe46c10c45
h0o0n <khnam022@naver.com>
2023-04-17 15:20:53 +0900

    C# 교과서 마스터하기 37  37. 메서드와 매개 변수

---
+++ C#/Operator/Operator/메서드와매개변수/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Operator.메서드와매개변수
+{
+    class Car
+    {
+        public void Map(params string[] title)
+        {
+            foreach(var t in title)
+            {
+                Console.WriteLine(t);
+            }
+        }
+
+    }
+}
---
+++ C#/Operator/Operator/메서드와매개변수/MethodAndParameter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator.메서드와매개변수
+{
+    class MethodAndParameter
+    {
+        public void main()
+        {
+            var car = new Car();
+            car.Map("홍길동","백두산");
+
+        }
+    }
+}
