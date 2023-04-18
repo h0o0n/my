b39af97eba5113f64e894d53f7923f318ee2551d
h0o0n <khnam022@naver.com>
2023-04-19 08:29:03 +0900

    C# 교과서 마스터하기 42 42. 클래스(Class) 기타, 부분 클래스, 메서드 체이닝, 불변 형식

---
+++ C#/Operator/Operator/부분클래스,메서드체이닝/Bye.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Operator.부분클래스_메서드체이닝
+{
+    public partial class Hello
+    {
+        public void Bye() => System.Console.WriteLine("Hello.cs");
+    }
+
+}
---
+++ C#/Operator/Operator/부분클래스,메서드체이닝/PartialClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator.부분클래스_메서드체이닝
+{
+    class PartialClass
+    {
+
+        public void main()
+        {
+            var hello = new Hello();
+            hello.Hi();
+            hello.Bye();
+        }
+    }
+}
