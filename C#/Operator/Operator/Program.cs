4c9d16ec5a75c5953d76a618ee4b57f75703a12c
h0o0n <khnam022@naver.com>
2023-04-19 08:30:44 +0900

    C# 교과서 마스터하기 47 47. 개체와 개체 지향 프로그래밍 요약 설명

--- C#/Operator/Operator/Program.cs
+++ C#/Operator/Operator/Program.cs
@@ -11,6 +11,12 @@ using Operator.열거형;
 using Operator.클래스;
 using Operator.알고리즘;
 using Operator.객체;
+using Operator.생성자;
+using Operator.메서드와매개변수;
+using Operator.속성사용;
+using Operator.대리자;
+using Operator.이벤트와_이벤트처리기;
+using Operator.상속;
 
 namespace Operator
 {
@@ -73,8 +79,41 @@ namespace Operator
             ag.algo();*/
 
             Operator.객체.Cars obj = new Operator.객체.Cars();
-            Console.WriteLine(obj);
-            obj.Go();
+            /*Console.WriteLine(obj);
+            obj.Go();*/
+
+            /* Operator.소멸자.Destructor des = new 소멸자.Destructor();
+             des.main1();*/
+
+            /*Operator.메서드와매개변수.MethodAndParameter map = new MethodAndParameter();
+            map.main();*/
+
+            /*Operator.속성사용.Property pro = new Property();
+            pro.main();*/
+
+            /*Operator.대리자.Delegate dg = new Operator.대리자.Delegate();
+            dg.main();*/
+
+            /*Operator.이벤트와_이벤트처리기.Event events = new Operator.이벤트와_이벤트처리기.Event();
+            events.main();*/
+
+            /*Operator.상속.InheritanceNote IN = new InheritanceNote();
+            IN.main();*/
+
+            /*Operator.오버라이드.OverrideDemo od = new 오버라이드.OverrideDemo();
+            od.main();*/
+
+            /*Operator.인터페이스.Interface it = new 인터페이스.Interface();
+            it.main();*/
+
+            /*Operator.특성과리플렉션.AttributeDemo abd = new 특성과리플렉션.AttributeDemo();
+            abd.main();*/
+
+            /*Operator.제네릭_클래스.GenericDemo gd = new 제네릭_클래스.GenericDemo();
+            gd.main();*/
+
+            Operator.확장메서드.ExtensionMethod em = new 확장메서드.ExtensionMethod();
+            em.main();
         }
         
     }
