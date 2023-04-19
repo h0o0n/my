a9b877d1608e56279fff1693845e74c6fbd8f6dc
h0o0n <khnam022@naver.com>
2023-04-19 09:06:34 +0900

    C# 교과서 마스터하기 50 50. 동적 형식

---
+++ C#/Operator/Operator/동적형식/동적형식.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operator.동적형식
+{
+    class 동적형식
+    {
+        public void main()
+        {
+            dynamic x;
+            x = 1_234;
+            Console.WriteLine($"{ x }");
+            Console.WriteLine($"{x.GetType()}");
+            x = "Dynamic Type!";
+            Console.WriteLine($"{ x }");
+            Console.WriteLine($"{x.GetType()}");
+        }
+    }
+}
