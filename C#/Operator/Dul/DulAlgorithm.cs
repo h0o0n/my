ea71d293601b98bab6f78d41faa5ca992a44a2b5
h0o0n <khnam022@naver.com>
2023-04-21 14:02:58 +0900

    C# 교과서 마스터하기 54 54. NuGet 패키지

---
+++ C#/Operator/Dul/DulAlgorithm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dul
+{
+    public class Algorithm
+    {
+        public static int[] SelectionSort(int[] numbers)
+        {
+            int N = numbers.Length;
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = i + 1; j < N; j++)
+                {
+                    if (numbers[i] > numbers[j])
+                    {
+                        int temp = numbers[i]; numbers[i] = numbers[j]; numbers[j] = temp;
+                    }
+                }
+            }
+
+            return numbers;
+        }
+    }
+}
