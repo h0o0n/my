88e8657228b478e209068f7fb04f3d56e2c5c6ae
h0o0n <khnam022@naver.com>
2023-04-21 14:03:15 +0900

    C# 교과서 마스터하기 55 55. 스레드(Thread)

---
+++ C#/Operator/Operator/스레드/Thread.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Operator.스레드
+{
+    class Thread
+    {
+
+        public static void Hi() { Console.WriteLine("Hi"); }
+        
+
+        public void main()
+        {
+            lock (this) 
+            {
+                Parallel.For(0, 200_000, (i) => { Console.WriteLine(i); });
+            }
+            
+        }
+    }
+}
