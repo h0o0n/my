0965294d15295d7ee70a38b25d8c4a7941a9ec32
h0o0n <khnam022@naver.com>
2023-04-21 14:02:20 +0900

    C# 교과서 마스터하기 53 53. 테스트 프로젝트와 단위 테스트

---
+++ C#/Operator/DulTests/00_Assert/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DulTests._00_Assert
+{
+    [TestClass]
+    public class AssertDemo
+    {
+        /*[TestMethod]
+        public void AssertEqualTest()
+        {
+            Assert.AreEqual(1234, 1234);
+        }*/
+
+        [TestMethod]
+        public void SelectionSort_ShouldReturnSortedArray()
+        {
+            int[] arr = { 33, 22, 44 };
+
+            //int[] results = Operator.클래스_라이브러리.Algorithm.SelectionSort(arr);
+
+            int[] results = DulAlgorithm.Algorithm.SelectionSort(arr);
+
+            Assert.AreEqual(22, results[0]);
+            Assert.AreEqual(44, results[results.Length - 1]);
+            foreach(int nums in results)
+            {
+                Console.WriteLine(nums);
+            }
+
+        }
+    }
+}
---
+++ C#/Operator/DulTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DulTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DulTests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fbece2d8-4c7c-4000-b97b-5a3ccd7dc7a6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
