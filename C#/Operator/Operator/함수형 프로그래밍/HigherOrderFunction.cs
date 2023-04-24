85fccf5a852133ad977bd7155d04d4e458702b58
h0o0n <khnam022@naver.com>
2023-04-24 11:41:11 +0900

    C# 교과서 마스터하기 61 61. 함수와 함수형 프로그래밍

---
+++ C#/Operator/Operator/함수형 프로그래밍/HigherOrderFunction.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
+
+namespace Operator.함수형_프로그래밍
+{
+    internal class HigherOrderFunction
+    {
+        
+
+        static void FuncitonParameterWithAction(Action<string> action, string message)
+        {
+            action(message);
+        }
+
+        Action<String> action = message => Console.WriteLine(message);
+
+        
+        
+        static void FunctionParameterWithFunc(Func<int,int>func, int number)
+        {
+            int result = func(number);
+            Console.WriteLine(  $"{number} * {number} = {result}");
+        }
+
+        Func<int,int> func = x => x*x;
+
+        static Action<string> FunctionReturnValueWithAction() =>
+            msg => Console.WriteLine($"{msg}" );
+
+        int[] numbers = { 1, 2, 3, 4, 5 };
+
+        public void main()
+        {
+            FuncitonParameterWithAction(action, "고차함수 : 매개변수");
+            FunctionParameterWithFunc(func, 3);
+            FunctionReturnValueWithAction();
+
+            //최댓값구하기
+
+            int max = numbers.Aggregate((f, s) => f > s ? f : s);
+            Console.WriteLine(max);
+
+            //최소값구하기
+            int min = numbers.Aggregate((f, s) => f < s ? f : s);
+            Console.WriteLine(min);
+        }
+
+
+
+    }
+}
