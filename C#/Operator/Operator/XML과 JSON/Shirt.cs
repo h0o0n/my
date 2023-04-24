093ef2c348501fa8612a177a34d2948b08cf780b
h0o0n <khnam022@naver.com>
2023-04-24 11:40:44 +0900

    C# 교과서 마스터하기 59  59. XML과 JSON

---
+++ C#/Operator/Operator/XML과 JSON/Shirt.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Operator.XML과_JSON
+{
+
+    public class Shirt
+    {
+        public string Name { get; set; }
+        public DateTime Created { get; set; }
+        public List<string> Sizes { get; set; }
+    }
+
+    internal class Shirts
+    {
+
+        public void main1()
+        {
+            Shirt shirt1 = new Shirt
+            {
+                Name = "Hoon Shirt",
+                Created = new DateTime(2020, 01, 01),
+                Sizes = new List<string> { "Small" }
+            };
+
+            string json1 = JsonConvert.SerializeObject(shirt1, Formatting.Indented);
+            Console.WriteLine(json1);
+
+            string json2 = @"{
+                'Name':'Black Shirt',
+                'Created':'2020-12-31T00:00:00',
+                'Sizes':['Large','Small']
+            }";
+
+            Shirt shirt2 = JsonConvert.DeserializeObject<Shirt>(json2);
+            Console.WriteLine(  $"{shirt2.Name}-{shirt2.Created}");
+        }
+    }
+
+   
+    
+}
---
+++ C#/Operator/Operator/XML과 JSON/XElementDemo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Operator.XML과_JSON
+{
+    internal class XElementDemo
+    {
+        public void main()
+        {
+            XElement category = new XElement("Menus",
+                new XElement("Menu", "책"),
+                new XElement("Menu", "강의"),
+                new XElement("Menu", "컴퓨터")
+                );
+            Console.WriteLine(category);
+
+            XElement newCategory = new XElement("Menus", 
+                from node in category.Elements()
+                where node.Value.ToString().Length >= 2
+                select node
+                );
+
+            Console.WriteLine(newCategory);
+        }
+    }
+}
