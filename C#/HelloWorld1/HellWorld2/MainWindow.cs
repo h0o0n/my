9fb8401d32ca04ed7cbd6a9ad0b6ff7ebc5d45e7
h0o0n <khnam022@naver.com>
2023-04-24 15:06:21 +0900

    C# WPF #1~#4 C# WPF #1 프로젝트만들기 C# WPF #2 스택패널 stackpanel C# WPF #3 그리드 Grid C# WPF #4 WrapPanel 랩패널과 StackPanel 스택패널차이

---
+++ C#/HelloWorld1/HellWorld2/MainWindow.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+
+namespace HellWorld2
+{
+    class MainWindow : Application
+    {
+        [STAThread]
+        public static void Main()
+        {
+            MainWindow app = new MainWindow();
+            app.Run();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Window mainWindow = new Window();   
+            mainWindow.Title = "WPF Sample(Main)";
+            mainWindow.MouseDown += WinMouseDown;
+            mainWindow.Show();
+
+            for(int i=0; i<2; i++)
+            {
+                Window win = new Window();
+                win.Title = "Extra Window No." + (i+1);
+                win.Show();
+            }
+        }
+
+        void WinMouseDown(object sender, MouseEventArgs e)
+        {
+            Window win = new Window();
+            win.Title = "Model Dialog Box";
+            win.Width = 400; win.Height = 200;
+
+            Button b = new Button();
+            b.Content = "Click Me!";
+            b.Click += Button_Click;  
+
+            win.Content = b;
+            win.ShowDialog();
+
+        }
+
+        void Button_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Button Click!", sender.ToString());
+        }
+    }
+}
---
+++ C#/Operator/CSharp.Nine/ChSharpNine.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Nine
+{
+    internal class ChSharpNine
+    {
+        static void Main()
+        {
+
+            int Fib(int i) =>
+            i switch
+            {
+                int when i <= 2 => 1,
+                _ => Fib(i - 2) + Fib(i - 1)
+            };
+
+            Console.WriteLine(  Fib(7));
+        }
+
+    }
+}
