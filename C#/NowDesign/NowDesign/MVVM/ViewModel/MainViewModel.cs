3e4a69afeb9c5ebe410dec35793de73db5f78c14
h0o0n <khnam022@naver.com>
2023-04-26 17:23:26 +0900

    C# 게시판 UI 만들기

---
+++ C#/NowDesign/NowDesign/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,32 @@
+﻿using ModernDesign.Core;
+using NowDesign.MVVM.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NowDesign.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            
+            { 
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel() 
+        { 
+            CurrentView = HomeView
+        }
+
+    }
+}
